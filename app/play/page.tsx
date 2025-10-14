c1296164f0b1b64cf25f56289e29d2ac6a99cb5b
Aman Fangeria <amanfangeria980@gmail.com>
2025-10-14 23:49:26 +0530

    chore: some more styling

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,14 +1,21 @@
+import GameBox from "@/components/game-box/GameBox";
 import React from "react";
+import { Text } from "../../components/retroui/Text";
 
 const GameScreen = () => {
     return (
         <div
-            className="flex flex-col items-center justify-center h-screen bg-cover bg-center"
+            className="flex flex-col items-center h-screen bg-cover bg-center"
             style={{ backgroundImage: "url('/assets/images/game.gif')" }}
         >
-            <h1 className="text-2xl font-bold text-white drop-shadow-lg">
-                2048 Game
-            </h1>
+            <div className="flex-[20%] flex items-center justify-center">
+                <Text as="h2" className="line-through">
+                    2048 Game
+                </Text>
+            </div>
+            <div className="flex-[80%] flex items-center justify-center">
+                <GameBox />
+            </div>
         </div>
     );
 };
---
+++ components/game-box/GameBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GameBox = () => {
+    return <div className="w-[350px] border-2 border-black rounded-md"></div>;
+};
+
+export default GameBox;
